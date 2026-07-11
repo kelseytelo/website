f2cc26acfcb1190b49e191ecd4fe05303443ba51
kelseytelo <kelseytelo@gmail.com>
2026-07-11 15:18:06 -0700

    Add Privacy back cover

--- photos.js
+++ photos.js
@@ -1,3 +1,3 @@
 window.archivePhotos = [
-  {"src": "images/library/processed/front/01-privacy-by-molly-young-front.png", "caption": "Privacy By Molly Young"},
+  {"src": "images/library/processed/front/01-privacy-by-molly-young-front.png", "caption": "Privacy By Molly Young", "backSrc": "images/library/processed/back/01-privacy-by-molly-young-back.png"},
 ];
