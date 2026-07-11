89859c6638e0003df8c5a9a622cc6b5bb7bbfc93
kelseytelo <kelseytelo@gmail.com>
2026-07-11 15:38:39 -0700

    Center captions and add Paris Review books

--- photos.js
+++ photos.js
@@ -1,3 +1,5 @@
 window.archivePhotos = [
   {"src": "images/library/processed/front/01-privacy-by-molly-young-front.png", "caption": "Privacy By Molly Young", "backSrc": "images/library/processed/back/01-privacy-by-molly-young-back.png"},
+  {"src": "images/library/processed/front/02-the-paris-review-254-front.png", "caption": "The Paris Review 254", "backSrc": "images/library/processed/back/02-the-paris-review-254-back.png"},
+  {"src": "images/library/processed/front/03-women-at-work-by-the-paris-review-front.png", "caption": "Women At Work By The Paris Review", "backSrc": "images/library/processed/back/03-women-at-work-by-the-paris-review-back.png"},
 ];
